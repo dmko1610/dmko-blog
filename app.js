829f342d81ebf214579c827be98a284524e50f0c
dmko1610 <trigger1610@gmail.com>
2019-08-01 00:45:46 +0400

    initial commit to dev branch

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const okta = require("@okta/okta-sdk-nodejs");
+const session = require("express-session");
+const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
+const blogRouter = require('./routes/blog');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+const app = express();
+const client = new okta.Client({
+  orgUrl: "https://dev-366955.okta.com",
+  token: "004H3OHKdlgFEvr-T3wboGvQKLmRRgVUPDxjNVihzF"
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+const oidc = new ExpressOIDC({
+  issuer: "https://dev-366955.okta.com/oauth2/default",
+  client_id: "0oa11bo5mceLSqQ64357",
+  client_secret: "nmmlxquGB99plV7suOoHLbWVG1Ng_SyoeXuDSTqZ",
+  redirect_uri: "http://localhost:3000/users/callback",
+  scope: "openid profile",
+  routes: {
+    login: {
+      path: "users/login"
+    },
+    callback: {
+      path: "users/callback ",
+      defaultRedirect: "/dashboard"
+    }
+  }
+});
+
+app.use(session({
+  secret: "sdfsdjafjkjJJFkkfdjkfsdgjhghueJJfiu",
+  resave: true,
+  saveUninitialized: false
+}));
+
+app.use(oidc.router);
+
+app.use((req, res, next) => {
+  if (!req.userinfo) return next();
+  client.getUser(req.userinfo.sub)
+    .then(user => {
+      req.user = user;
+      req.locals.user = user;
+      next();
+    });
+});
+
+// Routes
+app.use('/', blogRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
